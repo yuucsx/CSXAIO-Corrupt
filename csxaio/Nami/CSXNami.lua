fbfbbde50e0caff7a70c1c5e39aca389494c7f0b
0cJet <90260098+0cJet@users.noreply.github.com>
2022-08-24 18:50:42 -0400

    Added Utility Constructor

--- csxaio/Nami/CSXNami.lua
+++ csxaio/Nami/CSXNami.lua
@@ -1,6 +1,20 @@
 -- Settings table, will use this instead of retrieving menu value using :get() 
 local Settings = {} 
 
+
+-- Utility "class" constructor
+local Utility = setmetatable({}, 
+{
+    __call = function(self, ...)
+        local result = setmetatable({}, {__index = self})
+        result:init(...)
+
+        return result
+    end
+})
+
+
+
 -- Nami "class" constructor
 local Nami = setmetatable({}, 
 {
@@ -11,3 +25,4 @@ local Nami = setmetatable({},
         return result
     end
 })
+
