2c57fa5876c4eb5b71d5758ab6036bd0f2d9420d
0cJet <90260098+0cJet@users.noreply.github.com>
2022-08-25 11:52:39 -0400

    Update CSXNami.lua

--- csxaio/Nami/CSXNami.lua
+++ csxaio/Nami/CSXNami.lua
@@ -32,12 +32,17 @@ function Utility:GetDistance(p1, p2)
     return math.sqrt(self:GetDistanceSqr(p1, p2))
 end
 
-
+function Utility:GetLatency()
+    return game.latency / 2
+end
 
 function Utility:FilterEnemies(a,b)
     return a.health < b.health and a.characterIntermediate.armor < b.characterIntermediate.armor and a.spellBlock < b.spellBlock
 end
 
+function Utility:GetOrbwalkerTarget()
+    return orb.comboTarget
+end
 
 function Utility:GetTarget(range)
     range = range or myHero.characterIntermediate.attackRange
@@ -45,6 +50,19 @@ function Utility:GetTarget(range)
     return enemies[1]
 end
 
+function Utility:GetPrediction(target, inputTable)
+    local prediction = pred.getPrediction(target, inputTable)
+    return prediction
+end
+
+function Utility:CanCastSpell(spell)
+    local time = myHero:getSpell(spell).readyTime
+    return time and (time > 0 and time <= Utility:GetLatency()) 
+end
+
+function Utility:IsCastingSpell()
+    return myHero.activeSpell and myHero.activeSpell.castEndTime > game.time + Utility:GetLatency()
+end
 
 -- Input "class" constructor
 local Input = setmetatable({}, 
@@ -66,7 +84,11 @@ end
 
 function Input:CastSpell(spell, pos)
     if not spell then return end
-    if not pos then pos = myHero end 
+    if not pos then pos = myHero end
+
+    if not Utility:CanCastSpell(spell) then return end
+    if Utility:IsCastingSpell() then return end
+
     pos = pos ~= nil and (pos and pos.x or pos.pos)
 
     if spell == SpellSlot.W or spell == SpellSlot.E and pos.pos then
@@ -84,39 +106,8 @@ end
 function Input:SendAttack(unit)
     if not unit then return end
 
-    return myHero:SendAttack(target)
+    return myHero:SendAttack(unit)
 end
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
 
 -- Nami "class" constructor
 local Nami = setmetatable({}, 
@@ -128,4 +119,55 @@ local Nami = setmetatable({},
         return result
     end
 })
+
+function Nami:Init()
+    self.Spells = {
+        Q = {
+            delay = 0.25 + 0.726,
+            speed = math.huge,
+            range = 850,
+            radius = 200,
+            type = spellType.circular,
+            collision = {
+                hero = SpellCollisionType.None,
+                minion = SpellCollisionType.None,
+                tower = SpellCollisionType.None,
+
+                flags = bit.bor(CollisionFlags.Windwall, CollisionFlags.Samira)
+            }
+        },
+        R = {
+            delay = 0.5,
+            speed = 850,
+            range = 2750,
+            radius = 500,
+            type = spellType.linear,
+            collision = {
+                hero = SpellCollisionType.None,
+                minion = SpellCollisionType.None,
+                tower = SpellCollisionType.None,
+
+                flags = bit.bor(CollisionFlags.Windwall, CollisionFlags.Samira, CollisionFlags.Braum)
+            }
+        }
+    }
+end
+
+function Nami:UseQ(target, pred)
+    if not pred then return Input:CastSpell(SpellSlot.Q, target.position) end
+
+    return Input:CastSpell(SpellSlot.Q, pred.position)
+end
+
+function Nami:Combo()
+    local target = Utility:GetOrbwalkerTarget()
+    if not target then return end
+
+    return self:UseQ(target, Utility:GetPrediction(target, self.Spells.Q))
+end
+
+
+
+
+
 
